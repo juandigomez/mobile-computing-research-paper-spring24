d900016790bf9c5dd7b29ec8a47425d0b1b99177
juandigomez <j.david116@utexas.edu>
2024-04-08 22:14:43 -0500

    Create Ex1_Swift2KT_ChatGPT_ConciseV2.kt

---
+++ Ex1_Swift2KT_ChatGPT_ConciseV2.kt
@@ -0,0 +1,22 @@
+fun <T : Comparable<T>> mergeSort(array: List<T>): List<T> =
+    if (array.size <= 1) array else {
+        val mid = array.size / 2
+        val (left, right) = array.subList(0, mid) to array.subList(mid, array.size)
+        merge(mergeSort(left), mergeSort(right))
+    }
+
+fun <T : Comparable<T>> merge(left: List<T>, right: List<T>): List<T> =
+    sequence {
+        var (lIndex, rIndex) = 0 to 0
+        while (lIndex < left.size || rIndex < right.size) {
+            yield((right.takeIf { lIndex >= left.size || (rIndex < right.size && it[rIndex] < left[lIndex]) } ?: left)[rIndex.takeIf { lIndex < left.size } ?: lIndex++])
+            if (lIndex == left.size) rIndex++ else if (rIndex == right.size || left[lIndex] <= right[rIndex]) lIndex++ else rIndex++
+        }
+    }.toList()
+
+// Example usage
+fun main() {
+    val unsortedArray = listOf(5, 2, 8, 1, 3)
+    val sortedArray = mergeSort(unsortedArray)
+    println(sortedArray) // Output: [1, 2, 3, 5, 8]
+}
