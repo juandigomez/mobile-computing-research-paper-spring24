4a8338f90e1db99f02398b39fab7eedce87c32f5
juandigomez <j.david116@utexas.edu>
2024-04-08 22:46:48 -0500

    Create EX3_Swift2Kotlin_ChatGPT_ConciseV3.kt

---
+++ EX3_Swift2Kotlin_ChatGPT_ConciseV3.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var charArray = mutableListOf('a', 'b', 'c', 'd', 'e')
+    println("Original array: $charArray")
+    
+    // Reverse the array
+    charArray.reverse()
+    println("Reversed array: $charArray")
+}
